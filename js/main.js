55365414d2f043fab2e4054281fd9226892af936
cvtk <olegcvetkov76@gmail.com>
2016-03-30 17:59:15 +0300

    add news

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
-$(document).ready(function(){
-        $('html').niceScroll({
-        	touchbehavior: true,
-        	mousescrollstep: 40,
-        	cursorcolor: '#ffc400',
-        	smoothscroll: true,
-        	zindex: 100,
-        	cursorborder: '1px soldi #000',
-        	hwacceleration: true
-        });
-        $('#sectionFacts').imageScroll({
-			holderClass: 'section section-facts',
-			imgClass: 'section-facts__image'
-		});
+function initMap() {
+    var markers = [
+        {lat: 57.5947799, lng: 39.8840093},
+        {lat: 57.5781059, lng: 39.8443823},
+        {lat: 57.6498977, lng: 39.9483299},
+        {lat: 57.5842799, lng: 39.8698753}
+    ];
+
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+    center: markers[0],
+    scrollwheel: false,
+    zoom: 11
+  });
+
+    new google.maps.Marker({
+    map: map,
+    position: markers[0],
+    title: 'Центральный склад'
     });
+
+    new google.maps.Marker({
+    map: map,
+    position: markers[1],
+    title: 'Детское питание Кроха'
+    });
+
+    new google.maps.Marker({
+    map: map,
+    position: markers[2],
+    title: 'Детское питание "Заволга"'
+    });
+
+    new google.maps.Marker({
+    map: map,
+    position: markers[3],
+    title: 'Детское Питание "Суздальская Ярмарка"'
+    });
+}
+
+$(document).ready(function(){
+    $('#sectionFacts').imageScroll({
+		holderClass: 'section section-facts',
+		imgClass: 'section-facts__image'
+	});
+});
