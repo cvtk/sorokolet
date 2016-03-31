b2fd5e1b6e59bac4517357a37d4a5d55c3a5a5d9
cvtk <olegcvetkov76@gmail.com>
2016-03-31 10:59:57 +0300

    add anchors + corousel

--- js/main.js
+++ js/main.js
@@ -38,6 +38,47 @@ function initMap() {
 }
 
 $(document).ready(function(){
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
+        }
+    });
+
+    $('#partnersCarousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
+        });
     $('#sectionFacts').imageScroll({
 		holderClass: 'section section-facts',
 		imgClass: 'section-facts__image'
